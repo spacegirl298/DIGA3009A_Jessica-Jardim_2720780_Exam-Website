d7f5a63d0ebd9bdefbc742154c5fc06bb9e14c9d
spacegirl298 <jessicajardim2004@gmail.com>
2025-10-30 16:51:21 +0200

    About Us styling

--- About_Us/About_Us.js
+++ About_Us/About_Us.js
@@ -1,23 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cards = document.querySelectorAll(".card-inner");
 
-  cards.forEach(card => {
+  cards.forEach((card) => {
     let flipped = false;
-
     card.addEventListener("click", () => {
       flipped = !flipped;
       gsap.to(card, {
         duration: 0.8,
         rotateY: flipped ? 180 : 0,
-        ease: "power2.inOut"
+        scale: 1.05,
+        ease: "power2.inOut",
+        onComplete: () => gsap.to(card, { scale: 1, duration: 0.2 }),
       });
     });
   });
-});
-gsap.to(card, {
-  duration: 0.8,
-  rotateY: flipped ? 180 : 0,
-  scale: 1.05,
-  ease: "power2.inOut",
-  onComplete: () => gsap.to(card, { scale: 1, duration: 0.2 })
 });
